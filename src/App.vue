1ede464ae0a734bbd790c7920decbff31c978239
pt82 <62603226+pt82@users.noreply.github.com>
2024-04-01 13:53:16 +0700

    Update App.vue
    counter

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <div>
+      <img src='https://mc.yandex.ru/watch/96870628' style="position:absolute; left:-9999px;" alt="" />
+    </div>
     <router-view/>
   </div>
 </template>
