936e306f6843fd0c5a52ffead50b4adad020d0b3
pt82 <62603226+pt82@users.noreply.github.com>
2024-04-02 10:36:35 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
   <div id="app">
-    <div>
-      <img src='https://mc.yandex.ru/watch/96870628' style="position:absolute; left:-9999px;" alt="" />
-    </div>
     <router-view/>
   </div>
 </template>
