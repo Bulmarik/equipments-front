9f9edf415074b0493550f474d3e7ac1506bef200
pt82 <62603226+pt82@users.noreply.github.com>
2024-04-02 10:36:00 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -20,17 +20,17 @@ new Vue({
 /* eslint-disable */
 Vue.mixin({
   mounted() {
-  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}
-     m[i].l=1*new Date()
-     for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return }}
-     k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-     (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym")
+ (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}
+   m[i].l=1*new Date()
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-     ym(96909998, "init", {
-          clickmap:true,
-          trackLinks:true,
-          accurateTrackBounce:true
-     })
+   ym(96870628, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   })
   }
 })
 /* eslint-enable */
